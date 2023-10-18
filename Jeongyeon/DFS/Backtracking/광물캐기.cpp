a3cf0ea9cf8c5c6553712bd6413eebd18394220d
jystella17 <42269221+jystella17@users.noreply.github.com>
2023-10-18 21:00:43 +0900

    Update 광물캐기.cpp

--- Jeongyeon/DFS/Backtracking/광물캐기.cpp
+++ Jeongyeon/DFS/Backtracking/광물캐기.cpp
@@ -18,7 +18,7 @@ void mining(int fatigue, int start, int cnt){
         return;
     }
     
-    // 현재 사용할 곡괭이 선택 & 탐색
+    // 다음에 사용할 곡괭이 선택 & 탐색
     for(int i=0; i<3; i++){
         if(pick[i] == 0) continue;
         
